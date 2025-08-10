54d0c6c36099f62e0638683ddc30af91c5c60be7
Yunusov <abubakar.yunusov.47@mail.ru>
2025-08-10 20:52:56 +0300

    second

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,42 +6,52 @@ const userService = require("../service/userService")
 
 class authController {
   async regist(req, res) {
+    try {
+      const { login, password } = req.body
 
-    const { login, password } = req.body
+      const isRegistUser = await userService.searchByLogin(login)
 
-    const isRegistUser = await userService.searchByLogin(login)
+      if (isRegistUser) return res.status(409).json("Пользователь уже существует")
 
-    if (isRegistUser) return res.status(409).json("Пользователь уже существует")
+      const avatarImageName =
+        (req.files && req.files.avatar) ?
+          fileService.addImage(req.files.avatar) :
+          process.env.NO_AVATAR
 
-    const avatarImageName =
-      (req.files && req.files.avatar) ?
-        fileService.addImage(req.files.avatar) :
-        process.env.NO_AVATAR
+      const hashPassword = passwordService.hash(password)
 
-    const hashPassword = passwordService.hash(password)
+      const newUser = await userService.create({ ...req.body, avatar: avatarImageName, password: hashPassword })
 
-    const newUser = await userService.create({ ...req.body, avatar: avatarImageName, password: hashPassword })
+      res.json(newUser)
+    } catch (error) {
+      res.status(500).json("Не удалось загестрироваться, повторите попытку позже")
+    }
 
-    res.json(newUser)
+
   }
 
   async authorization(req, res) {
-    const { login, password } = req.body
 
-    const user = await userService.searchByLogin(login)
+    try {
+      const { login, password } = req.body
+
+      const user = await userService.searchByLogin(login)
 
 
-    if (!user) return res.status(401).json("Неверный логин или пароль")
+      if (!user) return res.status(401).json("Неверный логин или пароль")
 
-    const isValidPassword = passwordService.verify(password, user.password)
+      const isValidPassword = passwordService.verify(password, user.password)
 
-    if (!isValidPassword) return res.status(401).json("Неверный логин или пароль")
+      if (!isValidPassword) return res.status(401).json("Неверный логин или пароль")
 
-    const { name, data, avatar, _id } = user
+      const { name, data, avatar, _id } = user
 
-    const token = authService.createToken({ id: _id, name, data, avatar }, process.env.SECRET_KEY, "1h")
+      const token = authService.createToken({ id: _id, name, data, avatar }, process.env.SECRET_KEY, "1h")
 
-    res.json({ token })
+      res.json({ token })
+    } catch (error) {
+      res.status(500).json("Не удалось авторизоваться, повторите попытку позже")
+    }
 
   }
 }
